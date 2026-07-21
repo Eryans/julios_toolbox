893711634688aeed71b71dd35a8830766772e725
Julio Punkito <julesnoir@hotmail.fr>
2026-07-21 16:11:08 +0200

    add interactible base script, health component do not interact with hurtbox anymore

--- combat/health_component.gd
+++ combat/health_component.gd
@@ -2,19 +2,22 @@ extends Node
 class_name HealthComponent
 
 @export var max_hp: int
-@export var hurtbox: Hurtbox
 
 var current_hp: int
 
 signal health_depleted
 
 func _ready() -> void:
-    current_hp = max_hp
-    if hurtbox:
-        hurtbox.took_hit.connect(_on_took_hit)
+	current_hp = max_hp
 
-func _on_took_hit(_data: HitBox.AttackData) -> void:
-    current_hp -= 1
-    if current_hp <= 0:
-        health_depleted.emit()
-        hurtbox.took_hit.disconnect(_on_took_hit)
+func damage(value: int = 1) -> void:
+	current_hp -= value
+	check_if_dead()
+
+func kill() -> void:
+	current_hp = 0
+	check_if_dead()
+
+func check_if_dead() -> void:
+	if current_hp <= 0:
+		health_depleted.emit()
---
+++ interactibles/interacibles.gd
@@ -0,0 +1,6 @@
+extends Area3D
+class_name Interactable
+
+@warning_ignore_start("unused_parameter")
+func interact(data: Variant) -> void:
+    pass
